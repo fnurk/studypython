1180194fd4a478b47469caff37a5cdf44d53f4f8
Bram Stollman <freakor@gmail.com>
2013-05-07 21:31:29 +0200

    Adding my free for all file

---
+++ Freeforall/fweakout.py
@@ -0,0 +1,10 @@
+'''
+This is my free for all .py doc. Just some code I used to test my pi initially.
+'''
+
+temp = 0
+
+for x in range(1,101):
+	print "Iteration: " + str(x)
+	temp += x
+	print "Summation: " + str(temp)
